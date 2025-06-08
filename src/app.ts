d166210e66b39f2f9a5f84481f851c63cf7d5998
Dmytro Stoianov <dmytrostoianov05@gmail.com>
2025-06-08 16:46:39 +0300

    Setup simple websocket server

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,17 @@
-console.log('test')
+import { WebSocketServer } from 'ws'
+
+const PORT = 8080
+
+const wss = new WebSocketServer({ port: PORT })
+
+console.log(`Websocket server is listening on port: ${PORT}`)
+
+wss.on('connection', function connection(ws) {
+    ws.on('error', console.error)
+
+    ws.on('message', function message(data) {
+        console.log('received: %s', data)
+    })
+
+    ws.send('something')
+})
